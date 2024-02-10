5ede54630ae3e7dd8ccbf482d7c294ec128074b1
vsuptl <128426685+vsuptl@users.noreply.github.com>
2024-02-10 11:00:47 -0600

    Made changes to sign up

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -10,7 +10,6 @@ const Signup = () => {
     event.preventDefault(); // Prevents the default form submission behavior
     const email = event.target.username.value;
     const password = event.target.password.value;
-    const confirmPass = event.target.confirmPass.value;
 
     // Use createAcct from firebase.js to attempt creating a new user
     createAcct(email, password)
@@ -27,7 +26,7 @@ const Signup = () => {
   };
 
   return (
-    <div class="signupContainer">
+    <div class="loginContainer">
       <form onSubmit={handleSubmit}>
         <h3>Recipe Maker</h3>
 
@@ -37,8 +36,7 @@ const Signup = () => {
         <label htmlFor="password">Enter Password:</label>
         <input type="password" placeholder="Password (Min: 8 characters)" id="password" name="password" required />
 
-        <label htmlFor="confirmPass">Confirm Password:</label>
-        <input type="password" placeholder="Retype Password" id="confirmPass" name="confirmPass" required />
+        <br></br>
 
         <button type="submit">Register</button>
 
