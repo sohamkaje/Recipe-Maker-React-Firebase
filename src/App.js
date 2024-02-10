0734c46261c43f5a708207bf1ecd8d4e73b2ae00
vsuptl <128426685+vsuptl@users.noreply.github.com>
2024-02-09 18:54:06 -0600

    login info

--- src/App.js
+++ src/App.js
@@ -2,9 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Recipe Maker</h1>
+    <body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
     </div>
+    <form>
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" id="username"></input>
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" id="password"></input>
+
+        <button>Log In</button>
+        <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+    </form>
+</body>
+
   );
   
 }
