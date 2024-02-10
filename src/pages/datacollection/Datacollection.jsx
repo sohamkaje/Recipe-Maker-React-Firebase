f41150b4e209e9e8ba698c61cb41b37d6ea827c8
vsuptl <128426685+vsuptl@users.noreply.github.com>
2024-02-10 02:07:36 -0600

    worked on datacollection page

--- src/pages/datacollection/Datacollection.jsx
+++ src/pages/datacollection/Datacollection.jsx
@@ -3,7 +3,80 @@ import './datacollection.css';
 
 const datacollection = () => {
   return (
-    <h1>Choose Your Preferences</h1>
+    <div class co>
+      <h1>Choose Your Preferences</h1>
+      <div class="dietBox">
+        <h2>Diet</h2>
+        <select id='dietpreferences'>
+          <option>Balanced</option>
+          <option>High-Fiber</option>
+          <option>high-protein</option>
+          <option>low-carb</option>
+          <option>low-fat</option>
+          <option>low-sodium</option>
+        </select>
+      </div>
+      <div class="healthBox">
+        <h2>Health</h2>
+        <button>alcohol-free</button>
+        <button>dairy-free</button>
+        <button>egg-free</button>
+        <button>fish-free</button>
+        <button>gluten-free</button>
+        <button>keto-friendly</button>
+        <button>kidney-friendly</button>
+        <button>low-fat-abs</button>
+        <button>low-potassium</button>
+        <button>low-sugar</button>
+        <button>no-oil-added</button>
+        <button>peanut-free</button>
+        <button>pescatarian</button>
+        <button>pork-free (halal)</button>
+        <button>red-meat-free</button>
+        <button>sesame-free</button>
+        <button>soy-free</button>
+        <button>sugar-conscious</button>
+        <button>tree-nut-free</button>
+        <button>vegan</button>
+        <button>vegetarian</button>
+        <button>wheat-free</button>
+      </div>
+      <div class="cuisineType">
+        <h2>Cuisine</h2>
+        <select id='cuisinepreferences'>
+          <option>American</option>
+          <option>Asian</option>
+          <option>Caribbean</option>
+          <option>Central Europe</option>
+          <option>Chinese</option>
+          <option>Eastern Europe</option>
+          <option>French</option>
+          <option>Indian</option>
+          <option>Italian</option>
+          <option>Japanese</option>
+          <option>Mediterranean</option>
+          <option>Mexican</option>
+          <option>Middle Eastern</option>
+          <option>Nordic</option>
+          <option>South American</option>
+          <option>South East Asian</option>
+        </select>
+      </div>
+      <div class="mealTypeBox">
+        <h2>Diet</h2>
+        <select id='mealTypepreferences'>
+          <option>Balanced</option>
+          <option>High-Fiber</option>
+          <option>high-protein</option>
+          <option>low-carb</option>
+          <option>low-fat</option>
+          <option>low-sodium</option>
+        </select>
+      </div>
+      <div class="submitBox">
+        <button>Generate Recipe</button>
+      </div>
+    </div>
   )
 }
 
